b7dc94dfce7c862d73cd7d2eab2c99e40c630fc4
kutluhangil <kutluhangul@windowslive.com>
2025-12-27 04:05:30 +0300

    Task-2.js Updated

--- js/task-2.js
+++ js/task-2.js
@@ -11,18 +11,6 @@ const images = [
     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
     alt: "Group of Horses Running",
   },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  }
 ];
 
 const gallery = document.querySelector(".gallery");
@@ -31,8 +19,8 @@ const markup = images
   .map(
     ({ url, alt }) =>
       `<li class="gallery-item">
-        <img src="${url}" alt="${alt}" class="gallery-image">
-      </li>`
+         <img src="${url}" alt="${alt}" />
+       </li>`
   )
   .join("");
 
